dd03c448fe5b5fadc34378f02761aa12146445b4
siamcodes <siamcodes@gmail.com>
2020-07-16 10:03:38 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Route} from 'react-router-dom';
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import About from './components/About';
+import Contact from './components/Contact';
+import Home from './components/Home';
+import Profile from './components/Profile';
+import Signin from './components/Signin';
+import Signup from './components/Signup';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+        <Route exact={true} path="/" component={Home} />
+        <Route path="/about" component={About}/>
+        <Route path="/contact" component={Contact}/>
+        <Route path="/profile" component={Profile}/>
+        <Route path="/signin" component={Signin}/>
+        <Route path="/signup" component={Signup}/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+    NavbarText
+} from 'reactstrap';
+
+const Header = (props) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggle = () => setIsOpen(!isOpen);
+
+    return (
+        <div>
+            <Navbar color="light" light expand="md">
+                <NavbarBrand><Link to="/">Somkiat.J</Link></NavbarBrand>
+                <NavbarToggler onClick={toggle} />
+                <Collapse isOpen={isOpen} navbar>
+                    <Nav className="mr-auto" navbar>
+                        <NavItem>
+                            <NavLink><Link to="/about">เกี่ยวกับเรา</Link></NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink><Link to="/contact">ติดต่อเรา</Link></NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink><Link to="/profile">ข้อมูลส่วนตัว</Link></NavLink>
+                        </NavItem>                
+                    </Nav>
+
+                    <Nav className="ml-auto" navbar>
+                        <NavItem>
+                            <NavLink><Link to="/signin">เข้าสู่ระบบ</Link></NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink><Link to="/signup">สมัครสมาชิก</Link></NavLink>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
+            </Navbar>
+
+        </div>
+    )
+}
+
+export default Header;
